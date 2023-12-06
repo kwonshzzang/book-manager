1aeded81dc284478da77e3f3b33ea1bf3bbb3df9
kwonshzzang <kwonshzzang@gmail.com>
2023-12-06 20:14:04 +0900

    First Commit

---
+++ src/test/java/kr/co/kwonshzzang/bookmanager/bookmanager/BookmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.bookmanager.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
