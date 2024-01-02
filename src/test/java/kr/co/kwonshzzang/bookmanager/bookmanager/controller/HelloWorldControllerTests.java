4349e81e9e1d93602b746bad248b2f0e720db772
kwonshzzang <kwonshzzang@gmail.com>
2024-01-02 18:58:50 +0900

    Add HelloWorldControllerTests

--- src/test/java/kr/co/kwonshzzang/bookmanager/bookmanager/controller/HelloWorldControllerTests.java
+++ src/test/java/kr/co/kwonshzzang/bookmanager/bookmanager/controller/HelloWorldControllerTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -16,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 //@WebMvcTest
 @SpringBootTest
